62064d92ae64698bfab6efaf9a904a344dbd4472
Kalken12 <82832370+Kalken12@users.noreply.github.com>
2021-06-10 09:36:01 +0530

    A program in C to display the first 10 natural numbers.

---
+++ 1.Natural_No.c
@@ -0,0 +1,19 @@
+
+/*
+1. Write a program in C to display the first 10 natural numbers.
+Expected Output :
+1 2 3 4 5 6 7 8 9 10
+*/
+
+#include <stdio.h>
+
+
+void main()
+{
+	int  num;
+	for(num=1; num<=10; num++)
+	{
+		printf("%d\n", num);
+	}
+	
+}
